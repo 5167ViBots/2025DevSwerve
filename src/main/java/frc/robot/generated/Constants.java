b488d6283f8cd8005d171135cd15c1b2c721d962
InsertUsername765 <calebsorrell584@gmail.com>
2025-02-26 15:58:44 -0500

    Updated Constants
    constants have been update....d..

--- src/main/java/frc/robot/generated/Constants.java
+++ src/main/java/frc/robot/generated/Constants.java
@@ -4,30 +4,30 @@ public class Constants {
     public static final String RioCanBus = "rio";
 
     public static class LiftSubsystemConstants {
-    public static final int liftMotorID1 = 9;
+    public static final int liftMotorID1 = 7;
     public static final String liftMotor1Can = RioCanBus;
 
-    public static final int liftMotorID2 = 10;
+    public static final int liftMotorID2 = 5;
     public static final String liftMotor2Can = RioCanBus;
     }
 
     public static class IntakeSubsystemConstants {
     
-    public static final int coralIntake = 11;
+    public static final int coralIntake = 32;
     public static final String coralIntakeCan = RioCanBus;
 
-    public static final int topAlgaeIntake = 12;
+    public static final int topAlgaeIntake = 33;
     public static final String topAlgaeIntakeCan = RioCanBus;
         
-    public static final int bottomAlgeaIntake = 13;
+    public static final int bottomAlgeaIntake = 8;
     public static final String bottomAlgeaIntakeCAN = RioCanBus;
 
-    public static final int bottomAlgeaIntakeSetter = 14;
+    public static final int bottomAlgeaIntakeSetter = 6;
     public static final String bottomAlgeaIntakeSetterCAN = RioCanBus;
     }
 
     public static class CageSubsystemConstants {
-    public static final int cageMotor = 15;
+    public static final int cageMotor = 4;
     public static final String cageMotorCAN = RioCanBus;    
     }
 }
